2fbe24b66d0b408c3310d731179818a240b5d608
Riley Teige <riley.teige@gmail.com>
2014-11-01 23:34:16 -0500

    Cleaup.

--- src/Main.hs
+++ src/Main.hs
@@ -29,21 +29,23 @@ playTurns (GameState board currentPlayer _)
   | isVictory = return $ GameState board nextPlayer Victory
   | isDraw    = return $ GameState board nextPlayer Draw
   | otherwise = do
-      putStrLn "\n\nCURRENT BOARD:"
+      putStrLn "\n"
+      putStrLn $ "CURRENT PLAYER: " ++ show currentPlayer
+      putStrLn "CURRENT BOARD:"
       print board
-      move <- readMove
+      move <- readMove board
       playTurns $ GameState (insert currentPlayer move board) nextPlayer Playing
   where nextPlayer = if currentPlayer == X then O else X
         isVictory  = hasStandardWin board
         isDraw     = hasStandardDraw board
 
-readMove :: IO Int
-readMove = do
+readMove :: Board -> IO Int
+readMove board = do
   moveStr <- prompt "Enter a row-biased 1-9 coordinate to play: "
   let move = readInt moveStr
-  if move `elem` [1..9]
+  if move `elem` [1..9] && isValidMove move board
      then return move
-     else readMove
+     else readMove board
 
 readInt :: String -> Int
 readInt str = read str :: Int
